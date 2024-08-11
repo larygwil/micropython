8561734996a3b1f3a901824dd87982e1e0b80089
garywill <garywill@disroot.org>
2024-08-11 13:50:06 +0800

    set_arrchannel

--- ports/esp32/usermod/dmaadc.c
+++ ports/esp32/usermod/dmaadc.c
@@ -28,6 +28,8 @@ static mp_obj_t dmaadc_init1(
 static MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(dmaadc_init1_obj, 4, 4, dmaadc_init1);
 // 用 MP_DEFINE_CONST_FUN_OBJ_X （X=0~3）来设置参数个数，最大3。4或以上要用另一个
 
+//------------------------------------------------------------------------
+
 static mp_obj_t dmaadc_see_init1()
 {
     mp_printf(&mp_plat_print, "read_len=%ld buf_size=%ld sample_freq=%ld channel_num=%ld\n", read_len, buf_size, sample_freq, channel_num);
@@ -35,12 +37,40 @@ static mp_obj_t dmaadc_see_init1()
 }
 static MP_DEFINE_CONST_FUN_OBJ_0(dmaadc_see_init1_obj, dmaadc_see_init1);
 
-
+//------------------------------------------------------------------------
+static mp_obj_t dmaadc_set_arrchannels( mp_uint_t n_args, const mp_obj_t *args )
+{
+    for (int i=0; i<n_args; i++)
+    {
+        static const adc_channel_t chTable[] = {ADC_CHANNEL_0, ADC_CHANNEL_1, ADC_CHANNEL_2, ADC_CHANNEL_3, ADC_CHANNEL_4};
+        adc_channel_t ch = chTable [ mp_obj_get_int( args[i] ) ];
+        arr_channels[i] = ch;
+    }
+    return mp_obj_new_int(0);
+}
+static MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(dmaadc_set_arrchannels_obj, 1, SOC_ADC_PATT_LEN_MAX, dmaadc_set_arrchannels);
+//------------------------------------------------------------------------
+static mp_obj_t dmaadc_see_arrchannels()
+{
+    mp_printf(&mp_plat_print, "Current channel_num = %ld\n", channel_num);
+    for (int i=0; i<SOC_ADC_PATT_LEN_MAX; i++)
+    {
+         mp_printf(&mp_plat_print, "n:%d ch_enum:%d\n", (i+1), (int)arr_channels[i] );
+    }
+    // mp_printf(&mp_plat_print, "\n", read_len, buf_size, sample_freq, channel_num);
+    return mp_obj_new_int(0);
+}
+static MP_DEFINE_CONST_FUN_OBJ_0(dmaadc_see_arrchannels_obj, dmaadc_see_arrchannels);
+//==========================================================================
 
 static const mp_rom_map_elem_t dmaadc_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_dmaadc) },
+    
     { MP_ROM_QSTR(MP_QSTR_init1), MP_ROM_PTR(&dmaadc_init1_obj) },
     { MP_ROM_QSTR(MP_QSTR_see_init1), MP_ROM_PTR(&dmaadc_see_init1_obj) },
+    
+    { MP_ROM_QSTR(MP_QSTR_set_arrchannels), MP_ROM_PTR(&dmaadc_set_arrchannels_obj) },
+    { MP_ROM_QSTR(MP_QSTR_see_arrchannels), MP_ROM_PTR(&dmaadc_see_arrchannels_obj) },
 };
 static MP_DEFINE_CONST_DICT(dmaadc_module_globals, dmaadc_module_globals_table);
 
