89ee3c173e1c25a0b0f2657290df0f9b8ea3162a
garywill <garywill@disroot.org>
2024-10-22 14:02:53 +0800

    mypm加：ledc_init()和ledc_stop() （发现使用过一次RC_FAST作PWM后再使用一次APB就解决休眠电流大问题，但仅是ledc_stop不解决）

--- ports/esp32/usermod/mypm.c
+++ ports/esp32/usermod/mypm.c
@@ -15,6 +15,7 @@
 #include "hal/log.h"
 #include "soc/clk_tree_defs.h"
 #include "esp_rom_uart.h"
+#include "driver/ledc.h"
 
 static mp_obj_t mypm_sleep_pd_config ( mp_obj_t domain_obj, mp_obj_t option_obj)
 {
@@ -300,6 +301,86 @@ static mp_obj_t mypm_wait_uart_tx(mp_obj_t uart_num_obj)
     return mp_const_none;
 }
 static MP_DEFINE_CONST_FUN_OBJ_1(mypm_wait_uart_tx_obj, mypm_wait_uart_tx);
+//-----------------------------
+static mp_obj_t mypm_ledc_init(mp_uint_t n_args, const mp_obj_t *args)
+{
+    uint32_t pin_id = mp_obj_get_int(args[0]);
+    uint32_t freq = mp_obj_get_int(args[1]);
+    uint32_t duty_percent  = mp_obj_get_int(args[2]);//0~100
+    uint32_t channel_id  = mp_obj_get_int(args[3]);// 0~5
+    uint32_t timer_id = mp_obj_get_int(args[4]); // 0~3
+    uint32_t reso = mp_obj_get_int(args[5]);   // 1~14
+    uint32_t clk_src = mp_obj_get_int(args[6]); // enum
+    
+    uint32_t duty = (int) ((float)duty_percent/100*(1<<reso) );
+    // 在 ESP32-C3 上，当通道绑定的定时器配置了其最大 PWM 占空比分辨率（ MAX_DUTY_RES ），通道的占空比不能被设置到 (2 ** MAX_DUTY_RES) 。否则，硬件内部占空比计数器会溢出，并导致占空比计算错误。
+    
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_LOW_SPEED_MODE,
+        .timer_num        = timer_id,
+        .duty_resolution  = reso,
+        .freq_hz          = freq,  
+        .clk_cfg          = clk_src
+    };
+    esp_err_t ret;
+    
+    ret = ledc_timer_config(&ledc_timer);
+    if (ret != ESP_OK)
+        return mp_const_false;
+    
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC_LOW_SPEED_MODE,
+        .channel        = channel_id,
+        .timer_sel      = timer_id,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = pin_id,
+        .duty           = duty, 
+        .hpoint         = 0
+    };
+    ret = ledc_channel_config(&ledc_channel);
+//     if (ret != ESP_OK)
+//         return mp_const_false;
+//     
+//     ret = ledc_set_duty(LEDC_LOW_SPEED_MODE, channel_id, duty);
+//     if (ret != ESP_OK)
+//         return mp_const_false;
+// 
+//     // Update duty to apply the new value
+//     ret = ledc_update_duty(LEDC_LOW_SPEED_MODE, channel_id);
+    return (ret == ESP_OK) ? mp_const_true : mp_const_false;
+
+}
+static MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mypm_ledc_init_obj, 7, 7, mypm_ledc_init);
+//----------------------------
+// ledc_timer_rst() ??
+static mp_obj_t mypm_ledc_stop(mp_obj_t channel_id_obj, mp_obj_t timer_id_obj)
+{
+    uint32_t channel_id = mp_obj_get_int(channel_id_obj);
+    uint32_t timer_id = mp_obj_get_int(timer_id_obj);
+    
+    esp_err_t ret;
+    
+    ret = ledc_stop(LEDC_LOW_SPEED_MODE, channel_id, 0);
+    if (ret != ESP_OK)
+        return mp_const_false;
+
+    
+    ret = ledc_timer_pause(LEDC_LOW_SPEED_MODE, timer_id);
+    if (ret != ESP_OK)
+        return mp_const_false;
+
+    ledc_timer_config_t ledc_timer = {
+        .deconfigure = true, 
+        .speed_mode       = LEDC_LOW_SPEED_MODE,
+        .timer_num        = timer_id,
+    };
+    ret = ledc_timer_config(&ledc_timer);
+
+    return (ret == ESP_OK) ? mp_const_true : mp_const_false;   
+}
+static MP_DEFINE_CONST_FUN_OBJ_2(mypm_ledc_stop_obj, mypm_ledc_stop);
 //========================================================
 
 // 所有函数要加进这里面
@@ -387,6 +468,14 @@ static const mp_rom_map_elem_t mypm_module_globals_table[] = {
     
     // 等待UART发完
     { MP_ROM_QSTR(MP_QSTR_wait_uart_tx), MP_ROM_PTR(&mypm_wait_uart_tx_obj) },
+    
+    // LEDC 常数
+    { MP_ROM_QSTR(MP_QSTR_LEDC_USE_APB_CLK), MP_ROM_INT(LEDC_USE_APB_CLK) },
+    { MP_ROM_QSTR(MP_QSTR_LEDC_USE_RC_FAST_CLK), MP_ROM_INT(LEDC_USE_RC_FAST_CLK) },
+    { MP_ROM_QSTR(MP_QSTR_LEDC_USE_XTAL_CLK), MP_ROM_INT(LEDC_USE_XTAL_CLK) },
+    // LEDC 函数
+    { MP_ROM_QSTR(MP_QSTR_ledc_init), MP_ROM_PTR(&mypm_ledc_init_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ledc_stop), MP_ROM_PTR(&mypm_ledc_stop_obj) },
 };
 static MP_DEFINE_CONST_DICT(mypm_module_globals, mypm_module_globals_table);
 
